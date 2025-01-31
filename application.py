a7ee0967f9229522f428a02d0f8a0f9f11ae3960
Marek Gmyrek <marek.gmyrek@hycom.digital>
2025-01-31 17:52:19 +0100

    new hello message

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World - this is a new message!'
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
